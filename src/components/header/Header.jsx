37d725cdb7f506c7a4052cccf2673308b7c87c76
Ravi Pansuriya <49712335+imravipansuriya@users.noreply.github.com>
2022-08-15 13:09:30 +0530

    about page changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className='container header_container'>
         <h5>Hello I'm</h5>
         <h1>Ravi Pansuriya</h1>
-        <h5 className='text-light'>FrontEnd Devloper</h5>
+        <h5 className='text-light'>UI-UX Designer / FrontEnd Devloper</h5>
         <CTA/> 
         <HeaderSocials/> 
         <div className='me'>
